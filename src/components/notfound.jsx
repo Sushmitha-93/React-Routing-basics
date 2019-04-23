2a8526135237f0e6d162ed96d794ece93c7d3b59
Sushmitha DT <dtsushmitha@gmail.com>
2019-04-23 21:41:36 +0530

    First commit

---
+++ src/components/notfound.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const Notfound = () => {
+  return <h1>Page not found</h1>;
+};
+
+export default Notfound;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//React router gives us 3 components - [Route,Link,BrowserRouter] which help us to implement the routing.
+// Import "Switch" component from react-router to help us render components only when path matches
+// otherwise fall on not-found component.
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+import Users from "./components/users";
+import Contact from "./components/contact";
+import Notfound from "./components/notfound";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//In each Route component, we need to pass 2 props - "path", "component"
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        {/*user "exact" because "/" is common in all and App component will get rendered in all paths */}
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route children={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
+
 serviceWorker.unregister();
